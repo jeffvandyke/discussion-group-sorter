a2e398f08adeeb39752d7ca43a9f7c48b7794ce8
Jeff VanDyke <jeffvd.a216@gmail.com>
2022-05-11 23:16:57 -0400

    Get counts

--- main.ts
+++ main.ts
@@ -2,9 +2,19 @@ import { readFromCsv } from "./src/import";
 
 (async () => {
     const csvFilename = process.argv[2];
-    if (!csvFilename) throw new Error('Usage: node ./main.js csv-file-name.csv');
-    const records = await readFromCsv(csvFilename);
-    console.log(records);
+    if (!csvFilename)
+        throw new Error("Usage: node ./main.js csv-file-name.csv");
+    const { students, topics } = await readFromCsv(csvFilename);
+    const topicsByPopularity = topics.map((t) => ({
+        topic: t,
+        count: students
+            .map((s) => s.chosenTopics.filter((tt) => tt === t).length)
+            .reduce((l, r) => l + r, 0),
+    })).sort((a, b) => b.count - a.count);
+    console.log({
+        topicsByPopularity,
+        sum: topicsByPopularity.reduce((prev, { count }) => prev + count, 0)
+    });
 })().then(
     () => console.log("done"),
     (err) => console.error(err)
