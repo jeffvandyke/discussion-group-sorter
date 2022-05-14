20d3529f825ed58f1f9f41824dde5393bedc1753
Jeff VanDyke <jeffvd.a216@gmail.com>
2022-05-14 01:28:36 -0400

    Move algorithm greenfield into src folder

--- main.ts
+++ main.ts
@@ -1,8 +1,7 @@
 import _ from "lodash";
+import { assignStudentsToGroups } from "./src/algorithm";
 import { readFromCsv } from "./src/import";
 import { Time } from "./src/types";
-import { makeTopicGroups } from "./src/topics";
-import { makeEmptyAssignments } from "./src/assignments";
 
 const times: Time[] = ["Tues", "Wed", "Thurs"];
 
@@ -11,20 +10,8 @@ const times: Time[] = ["Tues", "Wed", "Thurs"];
     if (!csvFilename)
         throw new Error("Usage: node ./main.js csv-file-name.csv");
     const { students, topics } = await readFromCsv(csvFilename);
-    const topicsByPopularity = topics
-        .map((t) => ({
-            topic: t,
-            count: students
-                .map((s) => s.chosenTopics.filter((tt) => tt === t).length)
-                .reduce((l, r) => l + r, 0),
-        }))
-        .sort((a, b) => a.count - b.count);
 
-    const groups = makeTopicGroups(topicsByPopularity, times);
-
-    const randomizedStudents = _.sortBy(students, Math.random);
-
-    const studentAssignments = makeEmptyAssignments(randomizedStudents, times);
+    const studentAssignments = assignStudentsToGroups(students, topics, times);
 
     console.log(studentAssignments.map((a) => a.assignments));
 })().then(_.noop, (err) => console.error(err));
